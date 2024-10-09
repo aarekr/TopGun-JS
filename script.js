6752b75cb8dd418716a0112cee369ef629bb487e
aarekr <aare212@hotmail.com>
2024-10-09 16:45:34 +0300

    Drawing asteroids

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-var canvas = document.getElementById('topgun');
-var context = canvas.getContext('2d');
+let canvas = document.getElementById('topgun');
+let context = canvas.getContext('2d');
 
 function draw_shooter(ctx, x, y, radius, options) {
     options = options || {};
@@ -34,6 +34,53 @@ function draw_shooter(ctx, x, y, radius, options) {
     ctx.fill();
     ctx.stroke();
     ctx.restore();
+    // alternative shooter
+    /*let x_2 = 100;
+    let y_2 = 550;
+    ctx.beginPath();
+    ctx.moveTo(x_2, y_2);
+    ctx.lineTo(x_2-10, y_2+20);
+    ctx.lineTo(x_2+10, y_2+20);
+    ctx.closePath();
+    ctx.fill();
+    ctx.stroke();
+    ctx.restore();*/
 }
 
+function draw_asteroid(ctx, radius, segments, options) {
+    options = options || {};
+    ctx.strokeStyle = options.stroke || 'white';
+    ctx.fillStyle = options.fill || 'black';
+    ctx.save();
+    ctx.beginPath();
+    for (let i=0; i < segments; i++) {
+        ctx.rotate(2 * Math.PI / segments);
+        //ctx.lineTo(radius, 0);
+        ctx.lineTo(radius * 0.8 + radius * 0.4 * Math.random(), 0);
+        //ctx.lineTo(radius + radius * options.noise * (Math.random() - 0.5), 0);
+    }
+    ctx.closePath();
+    ctx.fill();
+    ctx.stroke();
+    if (options.guide) {
+        ctx.lineWidth = 0.5;
+        ctx.arc(0, 0, radius, 0, 2 * Math.PI);
+        ctx.stroke();
+    }
+    ctx.restore();
+}
+
+// drawing shooter
 draw_shooter(context, 270, 300, 30, {guide: true});
+
+// drawing asteroids
+let segments = 25, noise = 0.4;  // segments = corners
+for (let x=0.1; x<1; x+=0.2) {
+    for (let y=0.1; y<1; y+=0.2) {
+        context.save();
+        context.translate(context.canvas.width * x, context.canvas.height * y);
+        draw_asteroid(context, context.canvas.width / 12, segments, {noise: noise, guide: true});
+        context.restore();
+        noise += 0.025;
+    }
+}
