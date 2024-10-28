ac912a6cea7a3334fafd6d10042a9bcb4da6d084
aarekr <aare212@hotmail.com>
2024-10-28 13:38:23 +0200

    Refactoring, shooter finds test target and destroys it

--- animation.js
+++ animation.js
@@ -1,42 +1,7 @@
 let canvas = document.getElementById('topgun');
 let context = canvas.getContext('2d');
 
-// asteroid setup
-let segments = 24;
-let shape = [];
-for (let i=0; i < segments; i++) {
-    shape.push(Math.random() - 0.5);
-}
-let radius = 50;
-let noise = 0.2;
-let x = context.canvas.width * Math.random();
-let y = context.canvas.height * Math.random();
-let angle = 0;
-let x_speed = context.canvas.width * (Math.random() - 0.5);
-let y_speed = context.canvas.height * (Math.random() - 0.5);
-let rotation_speed = 2 * Math.PI * (Math.random() - 0.5);
-
-function draw(ctx, guide) {
-    ctx.save();
-    ctx.translate(x, y);
-    ctx.rotate(angle);
-    draw_asteroid(ctx, radius, shape, {guide: guide, noise: noise});
-    ctx.restore();
-}
-
 let previous, elapsed;
-function frame(timestamp) {
-    context.clearRect(0, 0, context.canvas.width, context.canvas.height);
-    if (!previous) previous = timestamp;
-    elapsed = timestamp - previous;
-    //draw(context, true);
-    console.log('X', elapsed);
-    previous = timestamp;
-    window.requestAnimationFrame(frame);
-}
-//window.requestAnimationFrame(frame);
-
-////////////////////////////////////////////////////////////////////////////////////
 
 function draw_asteroid(ctx, radius, segments) {
     ctx.strokeStyle = 'white';
@@ -68,58 +33,22 @@ function draw_gunship() {
     context.stroke();
 }
 
-let dx = 0
+let dx = 0;
 let dy_asteroid_dumb = 0;
+let dy_asteroid_static = 0;
 let dy_small_dumb = 0;
-//draw_gunship(dx, dy);
-/*for (let x=0.1; x<1; x+=1) {
-    for (let y=0.1; y<1; y+=1) {
-        context.save();
-        let random_x_location = Math.random() * 500;
-        let random_y_location = Math.random() * 100;
-        let random_radius = 5 + Math.random() * 15;
-        let random_segments = 10 + Math.random() * 10;
-        context.translate(random_x_location, random_y_location);
-        draw_asteroid(context, random_radius, random_segments);
-        context.restore();
-        noise += 0.025;
-    }
-}*/
-/*function create_asteroids() {
-    context.save();
-    let random_x_location = Math.random() * 500;
-    let random_y_location = Math.random() * 100;
-    let random_radius = 5 + Math.random() * 15;
-    let random_segments = 10 + Math.random() * 10;
-    context.translate(random_x_location, random_y_location);
-    draw_asteroid(context, random_radius, random_segments);
-    context.restore();
-}*/
 let random_x_location = Math.random() * 600;
 let asteroid_y_location = 0;
 let random_radius = 5 + Math.random() * 15;
 let random_segments = 10 + Math.random() * 10;
+
 function draw_asteroid_dumb(dy) {
     context.save();
     context.translate(random_x_location, asteroid_y_location + dy);
     draw_asteroid(context, random_radius, random_segments);
     context.restore();
 }
-/*class Asteroid {
-    Asteroid() {
-        this.x = 100,
-        this.y = 100,
-        this.radius = 30
-    }
-    getRadius() {
-        return this.radius;
-    }
-}
-let asteroid_object = new Asteroid();
-function draw_asteroid_object(asteroid_object) {
-    draw_asteroid(context, 20, 20);
-}
-draw_asteroid_object(asteroid_object)*/
+
 function Asteroid(segments, radius, noise) {
     this.x = context.canvas.width * Math.random();
     this.y = context.canvas.height * Math.random();
@@ -135,6 +64,33 @@ function Asteroid(segments, radius, noise) {
     }
 }
 
+function AsteroidStatic() {
+    this.x = 100;
+    this.y = 50;
+    this.show = true;
+    /*this.shape = [];
+    for (let i=0; i<7; i++) {
+        this.shape.push(Math.random() - 0.5);
+    }*/
+}
+let static_asteroid = new AsteroidStatic()
+function draw_asteroid_static(dy_asteroid_static) {
+    context.beginPath();
+    context.strokeStyle = '#FFFFFF';
+    context.fillStyle = '#FFFFFF';
+    context.lineWidth = 2;
+    context.moveTo(100+dy_asteroid_static, 100+dy_asteroid_static);
+    context.lineTo(110+dy_asteroid_static, 110+dy_asteroid_static);
+    context.lineTo(110+dy_asteroid_static, 120+dy_asteroid_static);
+    context.lineTo(100+dy_asteroid_static, 130+dy_asteroid_static);
+    context.lineTo(90+dy_asteroid_static, 130+dy_asteroid_static);
+    context.lineTo(80+dy_asteroid_static, 120+dy_asteroid_static);
+    context.lineTo(80+dy_asteroid_static, 110+dy_asteroid_static);
+    context.lineTo(90+dy_asteroid_static, 100+dy_asteroid_static);
+    context.fill();
+    context.stroke();
+}
+
 //let asteroidi = new Asteroid(24, 30, 0.2);
 //console.log(asteroidi);
 let asteroid_list = [
@@ -142,12 +98,6 @@ let asteroid_list = [
     new Asteroid(20, 30, 0.2),
     new Asteroid(40, 40, 0.2)
 ]
-function draw_asteroid_object(asteroidi) {
-    context.save();
-    context.translate(asteroidi.x, asteroidi.y);
-    draw_asteroid(context, asteroidi.radius, 24);
-    context.restore();
-}
 
 function draw_enemy_small_dumb(dy) {
     context.beginPath();
@@ -164,7 +114,7 @@ function draw_enemy_small_dumb(dy) {
 let enemyList = [[350,0], [400,0], [450,0]];
 function draw_three_small_dumb_enemies(dy) {
     for (let i=0; i<enemyList.length; i++) {
-        console.log('---> ', enemyList[i][0], enemyList[i][1])
+        //console.log('---> ', enemyList[i][0], enemyList[i][1])
         context.beginPath();
         context.strokeStyle = '#FFFFFF';
         context.fillStyle = '#FFFFFF';
@@ -200,8 +150,8 @@ function Bullet() {
     this.x = shooter.x;
     this.y = shooter.y;
 }
-let bullet = new Bullet();
-bulletList.push(bullet);
+//let bullet = new Bullet();
+//bulletList.push(bullet);
 function shoot_bullet() {
     let bullet = new Bullet();
     bulletList.push(bullet);
@@ -226,25 +176,12 @@ function handle_bullet_positions(bulletList) {
         draw_bullet(bulletList[i]);
     }
 }
-handle_bullet_positions(bulletList)
+//handle_bullet_positions(bulletList)
 
-function moveRight() {
-    //console.log('shooter position:', shooter.x, shooter.y);
-    return shooter.x += 10;
-}
-function moveLeft() {
-    //console.log('shooter position:', shooter.x, shooter.y);
-    return shooter.x -= 10;
-}
-function moveUp() {
-    //console.log('shooter position:', shooter.x, shooter.y);
-    return shooter.y -= 10;
-}
-function moveDown() {
-    //console.log('shooter position:', shooter.x, shooter.y);
-    return shooter.y += 10;
-}
-//function shootBullet() {}
+function moveRight() { return shooter.x += 10; }
+function moveLeft() { return shooter.x -= 10; }
+function moveUp() { return shooter.y -= 10; }
+function moveDown() { return shooter.y += 10; }
 
 window.onkeydown = function(e) {
     let key = e.key || e.keyCode;
@@ -287,25 +224,47 @@ function draw_test_target() {
 function check_collisions_bullets_items() {
     for (let i=bulletListIteratorFirst; i<bulletList.length; i++) {
         //console.log('bullet positions:', bulletList[i].x, bulletList[i].y);
-        if (bulletList[i].x > testTargetPosition.x && bulletList[i].x < testTargetPosition.x + 20) {
-            if (bulletList[i].y > testTargetPosition.y && bulletList[i].y < testTargetPosition.y + 20) {
-                console.log('a bullet hit the test target', bulletList[i].y);
-                showTestTarget = false;
+        if (showTestTarget == true) {
+            if (bulletList[i].x >= testTargetPosition.x && bulletList[i].x <= testTargetPosition.x + 20) {
+                if (bulletList[i].y >= testTargetPosition.y && bulletList[i].y <= testTargetPosition.y + 20) {
+                    console.log('a bullet hit the test target', bulletList[i].y);
+                    showTestTarget = false;
+                    bulletListIteratorFirst++;
+                }
             }
+        }
+        if (static_asteroid.show) {
+            console.log('static x,y:', static_asteroid.x, static_asteroid.y);
         }
     }
 }
 
+function guide_shooter() {
+    console.log('shooter-test_target:', shooter.x - testTargetPosition.x);
+    let difference = shooter.x - testTargetPosition.x;
+    if (difference <= 0) shooter.x++;
+    if (difference == 0) shoot_bullet();
+}
+
+let counter = 0;
 function frame(timestamp) {
     context.clearRect(0, 0, context.canvas.width, context.canvas.height);
     if (!previous) previous = timestamp;
     elapsed = timestamp - previous;
     //dx--;
     dy_asteroid_dumb += 2;
+    dy_asteroid_static += 0.0;
     dy_small_dumb++;
+    counter++;
+    if (counter%60 == 0) {
+        console.log('counter:', counter);
+        //counter = 0;
+    }
+    guide_shooter();
     //draw_gunship();  // original
     draw_shooter(shooter);
     if (showTestTarget == true) draw_test_target();
+    draw_asteroid_static(dy_asteroid_static);
     draw_asteroid_dumb(dy_asteroid_dumb);
     draw_enemy_small_dumb(dy_small_dumb);
     draw_three_small_dumb_enemies(dy_small_dumb);
